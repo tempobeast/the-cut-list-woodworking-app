fc02bf992eb9da3f4bda509084a2af236b9f82c9
Zach <shaw.zacht@gmail.com>
2022-10-21 12:47:49 -0700

    clear commented code

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,11 +2,6 @@ class UsersController < ApplicationController
 
     skip_before_action :authorize, only: :create
 
-    def index
-        users = User.all
-        render json: users, status: :ok
-    end
-
     def show
         user = User.find_by(id: session[:user_id])
         if user
