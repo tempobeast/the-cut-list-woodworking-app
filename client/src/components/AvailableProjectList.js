17d8bea367ba616bb34db9a09b11cc98a6549d63
Zach <shaw.zacht@gmail.com>
2022-10-22 23:16:48 -0700

    refactor

--- client/src/components/AvailableProjectList.js
+++ client/src/components/AvailableProjectList.js
@@ -4,20 +4,14 @@ import '../App.css'
 
 function AvailableProjectList({ projects, onProjectButtonClick, search, onProjectCardClick, user }) {
 
-    const allUserProjects = []
-    // user.projects.map((project) => allUserProjects.push(project.id))
-    user.followed_projects.map((project) => allUserProjects.push(project.id))
-    
-    const filterNonUserProjects = projects
-    .filter((project) => !allUserProjects.includes(project.id))
-    .filter((project) => project.title.toLowerCase().includes(search.toLowerCase()))
-
-
+    const filterProjects = projects.filter((project) => project.title.toLowerCase().includes(search.toLowerCase()))
 
     return (
         <div>
             <h2>Available Projects:</h2>
-            {filterNonUserProjects.map((project) => <ProjectCard all={true} project={project} key={project.id} onProjectButtonClick={onProjectButtonClick} userId={user.id} followed={false} onProjectCardClick={onProjectCardClick}/>)}
+            <div className="project-container">
+            {filterProjects.map((project) => <ProjectCard all={true} project={project} key={project.id} onProjectButtonClick={onProjectButtonClick} userId={user.id} followed={false} onProjectCardClick={onProjectCardClick}/>)}
+            </div>
         </div>
     )
 }
--- client/src/components/UserProjectList.js
+++ client/src/components/UserProjectList.js
@@ -4,16 +4,14 @@ import '../App.css'
 
 function UserProjectList({ user, onProjectButtonClick, userId, onUpdateProjectClick, onProjectCardClick, search }) {
 
-    const userProjects = [...user.projects, ...user.followed_projects]
-    const filterUserProjects = userProjects.filter((project) => project.title.toLowerCase().includes(search.toLowerCase()))
+    const filterUserProjects = user.user_related_projects.filter((project) => project.title.toLowerCase().includes(search.toLowerCase()))
 
-    
     return (
         <div>
             <h2>{user.username}'s Projects</h2>
-            {filterUserProjects.length > 0 ? filterUserProjects.map((project) => <ProjectCard project={project} key={project.id} onProjectButtonClick={onProjectButtonClick} userId={userId} onUpdateProjectClick={onUpdateProjectClick} onProjectCardClick={onProjectCardClick}/>) : <p>No projects match this search</p>}
-            {/* {user.projects.map((project) => <ProjectCard project={project} key={project.id} onProjectButtonClick={onProjectButtonClick} userId={userId} onUpdateProjectClick={onUpdateProjectClick} followed={false} onProjectCardClick={onProjectCardClick}/>)}
-            {user.followed_projects.map((project) => <ProjectCard project={project} key={project.id} onProjectButtonClick={onProjectButtonClick} userId={userId} followed={true} onProjectCardClick={onProjectCardClick}/>)} */}
+            <div className="project-container">
+                {filterUserProjects.length > 0 ? filterUserProjects.map((project) => <ProjectCard project={project} key={project.id} onProjectButtonClick={onProjectButtonClick} userId={userId} onUpdateProjectClick={onUpdateProjectClick} onProjectCardClick={onProjectCardClick}/>) : <p>No projects match this search</p>}
+            </div>
         </div>
     )
 }
