cb341e4a4d6ab4a93d03e13454dd1e7bdfb1c76b
Zach <shaw.zacht@gmail.com>
2022-10-25 15:40:29 -0700

    initial state set to empty string

--- client/src/context/projectToUpdate.js
+++ client/src/context/projectToUpdate.js
@@ -3,7 +3,7 @@ import React, { useState } from 'react'
 const ProjectToUpdateContext = React.createContext();
 
 function ProjectToUpdateProvider ({ children }) {
-    const [projectToUpdate, setProjectToUpdate] = useState({});
+    const [projectToUpdate, setProjectToUpdate] = useState('');
 
     return (
         <ProjectToUpdateContext.Provider value = {{ projectToUpdate, setProjectToUpdate }}>
