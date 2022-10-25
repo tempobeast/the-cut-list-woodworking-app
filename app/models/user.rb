9d29d0efcfb9f9bb4fc7325dba28e58ec037f1ae
Zach <shaw.zacht@gmail.com>
2022-10-24 22:07:14 -0700

    user projects and followed projects combined

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,8 +5,12 @@ class User < ApplicationRecord
     validates :username, presence: true, uniqueness: true
     validates :password, presence: true
 
-has_many :projects, dependent: :destroy
-has_many :follows, dependent: :destroy
-has_many :followed_projects, through: :follows, source: :project
+    has_many :projects, dependent: :destroy
+    has_many :follows, dependent: :destroy
+    has_many :followed_projects, through: :follows, source: :project
+
+    def user_related_projects
+        self.projects + self.followed_projects
+    end
 
 end
--- app/serializers/user_serializer.rb
+++ app/serializers/user_serializer.rb
@@ -1,8 +1,8 @@
 class UserSerializer < ActiveModel::Serializer
-  attributes :id, :username, :bio, :img_url
+  attributes :id, :username, :bio, :img_url, :user_related_projects
 
-  
+  has_many :user_related_projects
   has_many :follows
-  has_many :projects
-  has_many :followed_projects, serializer: FollowedProjectSerializer
+  # has_many :projects
+  # has_many :followed_projects, serializer: FollowedProjectSerializer
 end
