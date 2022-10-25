cd1b25f48cec58840c071152f26dec4eda394fd1
Zach <shaw.zacht@gmail.com>
2022-10-25 12:33:25 -0700

    #destroy and #create return updated user

--- app/controllers/follows_controller.rb
+++ app/controllers/follows_controller.rb
@@ -4,13 +4,14 @@ class FollowsController < ApplicationController
         user = find_user
         follow = user.follows.find(params[:id])
         follow.destroy
-        render json: follow, head: :no_content
+        projects = Project.where.missing(:follows).where.not(user_id: user.id) + Project.where(follows: Follow.where.not(user_id: user.id)).where.not(user_id: user.id)
+        render json: user, head: :no_content
     end
 
     def create
         user = find_user
         follow = user.follows.create!(follow_params)
-        render json: follow, status: :created
+        render json: user, status: :created
     end
 
     private
