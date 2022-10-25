128a24256a4dc134dcdda31d249432a96fac31ca
Zach <shaw.zacht@gmail.com>
2022-10-25 12:42:51 -0700

    serializes step and attached image

--- app/serializers/instruction_step_serializer.rb
+++ app/serializers/instruction_step_serializer.rb
@@ -1,6 +1,6 @@
 class InstructionStepSerializer < ActiveModel::Serializer
   include Rails.application.routes.url_helpers
-  attributes :id, :project_id, :step_number, :step_detail, :image_url
+  attributes :id, :project_id, :step_number, :step_detail, :image_url, :image
 
   def image
     if object.image.attached?
