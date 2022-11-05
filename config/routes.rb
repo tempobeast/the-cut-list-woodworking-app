08cb8210052d53fd7635fd38654dc850949b0465
Zach <shaw.zacht@gmail.com>
2022-11-04 22:37:18 -0700

    add update route to instruction_steps

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  resources :instruction_steps, :only => [:create, :index, :update]
+  resources :instruction_steps, :except => [:destroy]
   # resources :follows
   # resources :reviews
 
