3bd510cd1c3d6c87e5e5256b89ac4d3b660563f7
Zach <shaw.zacht@gmail.com>
2022-11-04 22:39:28 -0700

    removed byebug

--- app/controllers/instruction_steps_controller.rb
+++ app/controllers/instruction_steps_controller.rb
@@ -7,17 +7,39 @@ class InstructionStepsController < ApplicationController
     def create
         user = find_user
         if user
-            step = InstructionStep.create!(instruction_step_params)
-            render json: step, status: :created
+            project = user.projects.find(params[:project_id])
+            project.instruction_steps.create!({
+                step_detail: params[:step_detail],
+                image: params[:image],
+                step_number: project.instruction_steps.length + 1
+            })
+            project.instruction_steps.order(:id)
+            render json: project, status: :created
         else
             render json: { errors: ["Not Authorized"]}, status: :unauthorized
+        end
+    end
+
+    def update
+        user = find_user
+        if user 
+            project = user.projects.find(params[:project_id])
+            project.instruction_steps.find(params[:id]).update!(update_instruction_step_params)
+            project.instruction_steps.order(:id)
+            render json: project, status: :ok
+        else
+            render json: { errors: ["Not Authorized"], status: :unauthorized}
         end
     end
 
     private
 
     def instruction_step_params
-        params.permit(:step_number, :step_detail, :project_id, :image)
+        params.permit( :step_detail, :project_id, :image)
+    end
+
+    def update_instruction_step_params
+        params.permit( :step_detail, :image)
     end
 
 
