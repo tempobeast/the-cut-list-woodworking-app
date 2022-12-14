851ebfaf7c075ce3156c2e967dedf17343f98dbc
Zach <shaw.zacht@gmail.com>
2022-12-13 22:55:48 -0800

    conditional post or patch based on prop origin

--- client/src/components/ProjectInstructionsForm.js
+++ client/src/components/ProjectInstructionsForm.js
@@ -3,13 +3,13 @@ import { Input, Label, Textarea } from "../styles";
 import '../App.css';
 import { ProjectToUpdateContext } from "../context/projectToUpdate";
 
-function ProjectInstructionsForm({ stepToEdit, setEditStep }) {
+function ProjectInstructionsForm({ step, setEditStep, origin }) {
 
     const { projectToUpdate, setProjectToUpdate } = useContext(ProjectToUpdateContext)
-    const initialState = stepToEdit ? 
+    const initialState = step ? 
         {
-        step_detail: stepToEdit.step_detail,
-        // image: stepToEdit.image_url
+        step_detail: step.step_detail,
+        // image: step.image_url
         }
         :
         {
@@ -32,21 +32,28 @@ function ProjectInstructionsForm({ stepToEdit, setEditStep }) {
         const formData = new FormData();
         formData.append('project_id', projectToUpdate.id);
         formData.append('step_detail', instructionData.step_detail);
-        formData.append('image', instructionData.image);
-        submitFormData(formData)
+        if (instructionData.image) { 
+            formData.append('image', instructionData.image)
+        } 
+        submitNextStepFormData(formData);
+        if (step) {
+            setEditStep(false)
+        }
     }
 
-    function submitFormData(formData) {
-        fetch(stepToEdit ? `/instruction_steps/${stepToEdit.id}` : '/instruction_steps', {
-            method: stepToEdit ? "PATCH" : "POST",
+    function submitNextStepFormData(formData) {
+        fetch(step ? `/instruction_steps/${step.id}` : '/instruction_steps', {
+            method: step ? "PATCH" : "POST",
             body: formData,
         })
         .then((res) => {
             if (res.ok) {
                 res.json().then((updatedProject) => {
-                    setInstructionData(initialState)
-                    setProjectToUpdate(updatedProject)
-                    setEditStep(false)
+                    // const updatedStep = updatedProject.instruction_steps.find((newStep) => newStep.id === step.id)
+                    if (origin === "add_step") {
+                        setInstructionData(initialState);
+                    } 
+                    setProjectToUpdate(updatedProject);
                 })
             } else {
                 res.json().then((error) => console.log(error.error))
@@ -55,24 +62,30 @@ function ProjectInstructionsForm({ stepToEdit, setEditStep }) {
     }
 
     return (
-        <form onSubmit={onNextStepClick}>
-            <Label htmlfor="step-detail">Next Step:</Label>
-                <Textarea
-                type="textarea"
-                name="step_detail"
-                autoComplete="off"
-                value={instructionData.step_detail}
-                onChange={handleChange}
-                required/>
-            <Label htmlFor="step-image">Step Image:</Label>
-            <Input 
-                type="file"
-                accept="image/*"
-                multiple={false}
-                onChange={onImageChange}
-            />
-                <button>{stepToEdit ? "Update Step" : "Next Step"}</button>
-        </form>
+        <div>
+            <h1>{step ? `Step ${step.step_number}: `: "Add step"}</h1>
+        
+            <form onSubmit={onNextStepClick}>
+                <Label htmlfor="step-detail">Next Step:</Label>
+                    <Textarea
+                    type="textarea"
+                    name="step_detail"
+                    autoComplete="off"
+                    value={instructionData.step_detail}
+                    onChange={handleChange}
+                    required/>
+                <Label htmlFor="step-image">Step Image:</Label>
+                <Input 
+                    type="file"
+                    accept="image/*"
+                    multiple={false}
+                    onChange={onImageChange}
+                />
+                    <button onClick={onNextStepClick}>{step ? "Update Step" : "Next Step"}</button>
+                    {/* <button onClick={() => navigate(`/projects/${id}`)}>Submit Instructions</button> */}
+
+            </form>
+        </div>
     )
 }
 export default ProjectInstructionsForm
