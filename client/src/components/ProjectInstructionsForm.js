10d933610fb9c2c2598a0500e550d2ab9de6664a
Zach <shaw.zacht@gmail.com>
2022-12-14 11:15:53 -0800

    formatting

--- client/src/components/ProjectInstructionsForm.js
+++ client/src/components/ProjectInstructionsForm.js
@@ -1,91 +1,91 @@
 import React, { useState, useContext } from "react";
-import { Input, Label, Textarea } from "../styles";
-import '../App.css';
+import { Input, Label, Textarea, Button } from "../styles";
+import "../App.css";
 import { ProjectToUpdateContext } from "../context/projectToUpdate";
 
 function ProjectInstructionsForm({ step, setEditStep, origin }) {
-
-    const { projectToUpdate, setProjectToUpdate } = useContext(ProjectToUpdateContext)
-    const initialState = step ? 
-        {
+  const { projectToUpdate, setProjectToUpdate } = useContext(
+    ProjectToUpdateContext
+  );
+  // if updating existing instructionStep, initialState values are existing step values
+  // else initialState values are empty/null
+  const initialState = step
+    ? {
         step_detail: step.step_detail,
-        // image: step.image_url
-        }
-        :
-        {
+        image: step.image_url,
+      }
+    : {
         step_detail: "",
-        image: null
-        }
-    
-    const [instructionData, setInstructionData] = useState(initialState)
-    
-    function handleChange(e) {
-        setInstructionData({...instructionData, [e.target.name]: e.target.value})
+        image: null,
+      };
+
+  const [instructionData, setInstructionData] = useState(initialState);
+
+  function onStepDetailChange(e) {
+    setInstructionData({ ...instructionData, [e.target.name]: e.target.value });
+  }
+
+  function onImageChange(e) {
+    setInstructionData({ ...instructionData, image: e.target.files[0] });
+  }
+
+  //preps formData and sends to submitNextStep for POST or PATCH
+  function handleSubmitNextStep(e) {
+    e.preventDefault();
+    const formData = new FormData();
+    formData.append("project_id", projectToUpdate.id);
+    formData.append("step_detail", instructionData.step_detail);
+    if (instructionData.image) {
+      formData.append("image", instructionData.image);
     }
-
-    function onImageChange(e) {
-        setInstructionData({...instructionData, image: e.target.files[0]})
+    submitNextStep(formData);
+    if (step) {
+      setEditStep(false);
     }
+  }
 
-    function onNextStepClick(e) {
-        e.preventDefault();
-        const formData = new FormData();
-        formData.append('project_id', projectToUpdate.id);
-        formData.append('step_detail', instructionData.step_detail);
-        if (instructionData.image) { 
-            formData.append('image', instructionData.image)
-        } 
-        submitNextStepFormData(formData);
-        if (step) {
-            setEditStep(false)
-        }
-    }
+  //handles submit new POST and submit update PATCH based on existence of step prop
+  function submitNextStep(formData) {
+    fetch(step ? `/instruction_steps/${step.id}` : "/instruction_steps", {
+      method: step ? "PATCH" : "POST",
+      body: formData,
+    }).then((res) => {
+      if (res.ok) {
+        res.json().then((updatedProject) => {
+          if (origin === "add_step") {
+            setInstructionData(initialState);
+          }
+          setProjectToUpdate(updatedProject);
+        });
+      } else {
+        res.json().then((error) => console.log(error.error));
+      }
+    });
+  }
 
-    function submitNextStepFormData(formData) {
-        fetch(step ? `/instruction_steps/${step.id}` : '/instruction_steps', {
-            method: step ? "PATCH" : "POST",
-            body: formData,
-        })
-        .then((res) => {
-            if (res.ok) {
-                res.json().then((updatedProject) => {
-                    // const updatedStep = updatedProject.instruction_steps.find((newStep) => newStep.id === step.id)
-                    if (origin === "add_step") {
-                        setInstructionData(initialState);
-                    } 
-                    setProjectToUpdate(updatedProject);
-                })
-            } else {
-                res.json().then((error) => console.log(error.error))
-            }
-        })
-    }
-
-    return (
-        <div>
-            <h1>{step ? `Step ${step.step_number}: `: "Add step"}</h1>
-        
-            <form onSubmit={onNextStepClick}>
-                <Label htmlfor="step-detail">Next Step:</Label>
-                    <Textarea
-                    type="textarea"
-                    name="step_detail"
-                    autoComplete="off"
-                    value={instructionData.step_detail}
-                    onChange={handleChange}
-                    required/>
-                <Label htmlFor="step-image">Step Image:</Label>
-                <Input 
-                    type="file"
-                    accept="image/*"
-                    multiple={false}
-                    onChange={onImageChange}
-                />
-                    <button onClick={onNextStepClick}>{step ? "Update Step" : "Next Step"}</button>
-                    {/* <button onClick={() => navigate(`/projects/${id}`)}>Submit Instructions</button> */}
-
-            </form>
-        </div>
-    )
+  return (
+    <div>
+      <h1>{step ? `Step ${step.step_number}: ` : "Add step"}</h1>
+      <form onSubmit={handleSubmitNextStep}>
+        <Label htmlfor="step-detail">Next Step:</Label>
+        <Textarea
+          type="textarea"
+          name="step_detail"
+          autoComplete="off"
+          value={instructionData.step_detail}
+          onChange={onStepDetailChange}
+          required
+        />
+        <Label htmlFor="step-image">Step Image:</Label>
+        <Input
+          type="file"
+          accept="image/*"
+          multiple={false}
+          onChange={onImageChange}
+        />
+        <Button type="submit">{step ? "Update Step" : "Next Step"}</Button>
+      </form>
+    </div>
+  );
 }
-export default ProjectInstructionsForm
+export default ProjectInstructionsForm;
