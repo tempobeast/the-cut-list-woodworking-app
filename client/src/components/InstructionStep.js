bea4f3fa706d531f5f17cf189600eb9153b014d5
Zach <shaw.zacht@gmail.com>
2022-12-13 22:53:54 -0800

    removed callback for setEditStep

--- client/src/components/InstructionStep.js
+++ client/src/components/InstructionStep.js
@@ -1,24 +1,14 @@
-import React, { useState } from "react";
+import React from "react";
 import '../App.css';
-import ProjectInstructionsForm from "./ProjectInstructionsForm";
 
-function InstructionStep({step, index, cardStatus}) {
+function InstructionStep({step}) {
 
-    const [ editStep, setEditStep ] = useState(false)
-
-    if (!editStep) {
-        return (
-            <div key={step.id}>
-                <h4>Step {step.step_number}:</h4>
-                <p>{step.step_detail}</p>
-                <img src={step.image_url} alt={step.detail} className="step-image"/>
-                {cardStatus === "user_authored_project" ? <button onClick={() => setEditStep(true)}>edit step</button> : null}
-            </div>
-        )
-    } else {
-        return (
-            <ProjectInstructionsForm stepToEdit={step} index={index} setEditStep={setEditStep}/>
-        )
-    }
+     return (
+        <div>
+            <h4>Step {step.step_number}:</h4>
+            <p>{step.step_detail}</p>
+            <img src={step.image_url} alt={step.detail} className="step-image"/>
+        </div>
+    )
 }
 export default InstructionStep
