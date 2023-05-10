c6d81a3166b17f11d5a02c11996f3b81f4fb031e
Zach <shaw.zacht@gmail.com>
2023-05-10 14:24:06 -0700

    init component

---
+++ client/src/components/ToolsRequired.js
@@ -0,0 +1,17 @@
+import React from "react";
+import toolData from "../ToolData";
+import '../App.css';
+
+function ToolsRequired() {
+
+    const { setSearch } = useContext(SearchContext) 
+
+
+    return (
+        <div>
+            
+        </div>
+    )
+}
+
+export default ToolsRequired
