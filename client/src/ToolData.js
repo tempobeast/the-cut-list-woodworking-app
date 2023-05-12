c4e08f29e0effa2a2eb7beb793591a347afb7133
Zach <shaw.zacht@gmail.com>
2023-05-12 15:13:44 -0700

    temporary seed data

---
+++ client/src/ToolData.js
@@ -0,0 +1,104 @@
+const toolData = [
+    {
+        name: "Hammer",
+        image: "https://m.media-amazon.com/images/I/71tTWyypTKL._AC_UF894,1000_QL80_.jpg"
+    },
+    {
+        name: "Circular Saw",
+        image: "https://m.media-amazon.com/images/I/71RdFLNY-XL.jpg"
+    },
+    {
+        name: "Jigsaw",
+        image: "https://mobileimages.lowes.com/productimages/a1a482ae-a255-45d2-92f2-2bf235237007/09224763.jpg?size=pdhism"
+    },
+    {
+        name: "Orbital Sander",
+        image: "https://m.media-amazon.com/images/I/71FFNjdRR4S.jpg"
+    },
+    {
+        name: "Drill",
+        image: "https://images.thdstatic.com/productImages/de9bb7df-eb7d-4a9c-ad92-d76ace8fa4ba/svn/dewalt-power-drills-dcd780b-64_600.jpg"
+    },
+    {
+        name: "Table Saw",
+        image: "https://images.thdstatic.com/productImages/960b35aa-4b8a-4d13-a696-96cfb0d847e5/svn/dewalt-portable-table-saws-dwe7485-64_600.jpg"
+    },
+    {
+        name: "Band Saw",
+        image: "https://woodworker.com/images/SS/28-640.jpg"
+    },
+    {
+        name: "Hammer",
+        image: "https://m.media-amazon.com/images/I/71tTWyypTKL._AC_UF894,1000_QL80_.jpg"
+    },
+    {
+        name: "Circular Saw",
+        image: "https://m.media-amazon.com/images/I/71RdFLNY-XL.jpg"
+    },
+    {
+        name: "Jigsaw",
+        image: "https://mobileimages.lowes.com/productimages/a1a482ae-a255-45d2-92f2-2bf235237007/09224763.jpg?size=pdhism"
+    },
+    {
+        name: "Orbital Sander",
+        image: "https://m.media-amazon.com/images/I/71FFNjdRR4S.jpg"
+    },
+    {
+        name: "Drill",
+        image: "https://images.thdstatic.com/productImages/de9bb7df-eb7d-4a9c-ad92-d76ace8fa4ba/svn/dewalt-power-drills-dcd780b-64_600.jpg"
+    },
+    {
+        name: "Table Saw",
+        image: "https://images.thdstatic.com/productImages/960b35aa-4b8a-4d13-a696-96cfb0d847e5/svn/dewalt-portable-table-saws-dwe7485-64_600.jpg"
+    },
+    {
+        name: "Hammer",
+        image: "https://m.media-amazon.com/images/I/71tTWyypTKL._AC_UF894,1000_QL80_.jpg"
+    },
+    {
+        name: "Circular Saw",
+        image: "https://m.media-amazon.com/images/I/71RdFLNY-XL.jpg"
+    },
+    {
+        name: "Jigsaw",
+        image: "https://mobileimages.lowes.com/productimages/a1a482ae-a255-45d2-92f2-2bf235237007/09224763.jpg?size=pdhism"
+    },
+    {
+        name: "Orbital Sander",
+        image: "https://m.media-amazon.com/images/I/71FFNjdRR4S.jpg"
+    },
+    {
+        name: "Drill",
+        image: "https://images.thdstatic.com/productImages/de9bb7df-eb7d-4a9c-ad92-d76ace8fa4ba/svn/dewalt-power-drills-dcd780b-64_600.jpg"
+    },
+    {
+        name: "Table Saw",
+        image: "https://images.thdstatic.com/productImages/960b35aa-4b8a-4d13-a696-96cfb0d847e5/svn/dewalt-portable-table-saws-dwe7485-64_600.jpg"
+    },
+    {
+        name: "Hammer",
+        image: "https://m.media-amazon.com/images/I/71tTWyypTKL._AC_UF894,1000_QL80_.jpg"
+    },
+    {
+        name: "Circular Saw",
+        image: "https://m.media-amazon.com/images/I/71RdFLNY-XL.jpg"
+    },
+    {
+        name: "Jigsaw",
+        image: "https://mobileimages.lowes.com/productimages/a1a482ae-a255-45d2-92f2-2bf235237007/09224763.jpg?size=pdhism"
+    },
+    {
+        name: "Orbital Sander",
+        image: "https://m.media-amazon.com/images/I/71FFNjdRR4S.jpg"
+    },
+    {
+        name: "Drill",
+        image: "https://images.thdstatic.com/productImages/de9bb7df-eb7d-4a9c-ad92-d76ace8fa4ba/svn/dewalt-power-drills-dcd780b-64_600.jpg"
+    },
+    {
+        name: "Table Saw",
+        image: "https://images.thdstatic.com/productImages/960b35aa-4b8a-4d13-a696-96cfb0d847e5/svn/dewalt-portable-table-saws-dwe7485-64_600.jpg"
+    },
+]
+
+export default toolData
