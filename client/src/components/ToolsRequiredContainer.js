716fb6ceebd6f4715b80fae33afb4c0b0d58c5b1
Zach <shaw.zacht@gmail.com>
2023-05-14 21:16:41 -0700

    styling for tools required

--- client/src/components/ToolsRequiredContainer.js
+++ client/src/components/ToolsRequiredContainer.js
@@ -5,13 +5,15 @@ function ToolsRequiredContainer({toolsRequired}) {
 
     const selectedTools = toolsRequired.map((tool) => 
         <div key={tool.name} className="tool-required">
-            <img className="tool-required__image" src={tool.image} alt={tool.name}/>
+            <div className="tool-required__image-container">
+                <img className="tool-required__image" src={tool.image} alt={tool.name}/>
+            </div>
             <p className="tool-required__name">{tool.name}</p>
         </div>
     )
 
     return (
-        <div className="tools-required__container">
+        <div className="tools-required__selected">
             <ul>
                 {selectedTools}
             </ul>
