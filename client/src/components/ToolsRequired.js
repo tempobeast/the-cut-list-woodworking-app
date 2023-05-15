0d096bb028fc5d09e37ca83744712062cf885e0d
Zach <shaw.zacht@gmail.com>
2023-05-15 12:18:40 -0700

    get and add to list of tools for project requirements

--- client/src/components/ToolsRequired.js
+++ client/src/components/ToolsRequired.js
@@ -1,9 +1,43 @@
-import React from "react";
-import toolData from "../ToolData";
+import React, { useEffect, useState } from "react";
+// import toolData from "../ToolData";
 import '../App.css';
 import ToolsRequiredContainer from "./ToolsRequiredContainer";
 
 function ToolsRequired({toolsRequired, setToolsRequired}) {
+
+    const initialToolValue = {
+        name: '',
+        image: ''
+    }
+    const [ toolData, setToolData ] = useState([])
+    const [ addTool, setAddTool ] = useState(initialToolValue)
+
+    useEffect(() => {
+        fetch("/tools")
+        .then((res) => res.json())
+        .then((tools) => setToolData(tools))
+    }, [])
+    
+    function handleChange(e) {
+        setAddTool({...addTool,
+            [e.target.name]: e.target.value
+        })
+    }
+
+    function submitAddTool(e) {
+        fetch('/tools', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+            },
+            body: JSON.stringify(addTool)
+        })
+        .then((res) => res.json())
+        .then((newTool) => {
+            setToolData([...toolData, newTool]);
+            setAddTool(initialToolValue)
+        })
+    }
 
     const toolsToDisplay = toolData
         .sort((a,b) => a.name < b.name ? -1 : a.name > b.name ? 1 : 0 )
@@ -16,9 +50,9 @@ function ToolsRequired({toolsRequired, setToolsRequired}) {
                 <ul className="tools-list">
                     {toolsToDisplay}
                 </ul>
-                <input placeholder="add tool"></input>
-                <input placeholder="add tool image url"></input>
-                <button>x</button>
+                <input onChange={handleChange} name="name" placeholder="add tool" value={addTool.name}></input>
+                <input onChange={handleChange} name="image" placeholder="add tool image url" value={addTool.image}></input>
+                <button onClick={submitAddTool}>add</button>
             </div>
             <ToolsRequiredContainer toolsRequired={toolsRequired}/>
         </div>
