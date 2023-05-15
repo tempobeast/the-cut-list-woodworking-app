39aeb41c909a6452dfcb8257c90868e36a4750df
Zach <shaw.zacht@gmail.com>
2023-05-15 12:58:45 -0700

    init tools table

---
+++ db/migrate/20230515164954_create_tools.rb
@@ -0,0 +1,10 @@
+class CreateTools < ActiveRecord::Migration[6.1]
+  def change
+    create_table :tools do |t|
+      t.string :name
+      t.string :image
+
+      t.timestamps
+    end
+  end
+end
