f9b01f0e3fff110d835ec5da3b3350740ee1935f
Zach <shaw.zacht@gmail.com>
2023-05-23 15:52:13 -0700

    added classNames for styling

--- client/src/components/InstructionStep.js
+++ client/src/components/InstructionStep.js
@@ -4,7 +4,7 @@ import '../App.css';
 function InstructionStep({step}) {
 
      return (
-        <div>
+        <div className="instruction-step">
             <h4>Step {step.step_number}:</h4>
             <p>{step.step_detail}</p>
             <img src={step.image_url} alt={step.detail} className="step-image"/>
