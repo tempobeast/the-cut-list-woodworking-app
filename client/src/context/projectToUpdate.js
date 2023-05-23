9a89b0716ec7d6a163c1044659e15c29f0a4c37f
Zach <shaw.zacht@gmail.com>
2023-05-22 20:29:16 -0700

    project state initialized to an object

--- client/src/context/projectToUpdate.js
+++ client/src/context/projectToUpdate.js
@@ -3,7 +3,17 @@ import React, { useState } from 'react'
 const ProjectToUpdateContext = React.createContext();
 
 function ProjectToUpdateProvider ({ children }) {
-    const [projectToUpdate, setProjectToUpdate] = useState('');
+    const [projectToUpdate, setProjectToUpdate] = useState({
+        title: '', 
+        img_url: '', 
+        materials: '', 
+        time: '', 
+        description: '',
+        tools: '',
+        user_id: '',
+        creator: '',
+        instructions: '',
+    });
 
     return (
         <ProjectToUpdateContext.Provider value = {{ projectToUpdate, setProjectToUpdate }}>
